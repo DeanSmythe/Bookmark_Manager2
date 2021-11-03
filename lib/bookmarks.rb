b3f61657e5b62d24c0d1e88f7912cb3f5029f10c
Dean Smythe <dean@teamtwisted.co.uk>
2021-11-03 14:37:12 +0000

    Upgrading your toolset

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -7,7 +7,6 @@ class Bookmarks
   conn = PG.connect( dbname: 'bookmark_manager' )
   conn.exec( "SELECT * FROM bookmarks" ) do |result|
     result.each do |row|
-      puts row
       @bookmarks << row['url']
     end
   end
